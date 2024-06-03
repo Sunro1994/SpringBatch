733b6de1edde8851aa232fa1bedd2364be492817
Sunro Lee <tjsfhdhkd@naver.com>
2024-06-03 21:11:45 +0900

    firstCommit

---
+++ BatchPractice/src/test/java/com/sunro/BatchPractice/BatchPracticeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sunro.BatchPractice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchPracticeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
